d7901b7b9ab779babe5f165fcc567a11e60c536c
Austin Shawaga <60626400+Retr0-XX@users.noreply.github.com>
2022-03-22 22:50:46 -0600

    Added Sign Ups
    Added ability for admin employee to add a client or employee to the database.
    
    Fixed date formatting on output.

--- BackEnd/API/main.py
+++ BackEnd/API/main.py
@@ -30,7 +30,7 @@ def elogin():
         _id = _json['eId']
         _pw = _json['password']
     except:
-        return authenticationError()
+        return badRequest()
 
     if(type(_id) != int or type(_pw) != str):
         return authenticationError()
@@ -44,7 +44,7 @@ def elogin():
         try:
             conn = mysql.connect()
             cursor = conn.cursor(pymysql.cursors.DictCursor)
-            result = cursor.execute(f'SELECT eId,branchId,email,phoneNum,dob,firstName,lastName,sex,eType FROM employee WHERE eId={_id} AND passwordHash="{h}";')
+            result = cursor.execute(f'SELECT eId,branchId,email,phoneNum,DATE_FORMAT(dob,"%Y-%m-%d") as dob,firstName,lastName,sex,eType FROM employee WHERE eId={_id} AND passwordHash="{h}";')
             if(result <= 0):
                 return authenticationError()
             else: #found
@@ -107,6 +107,91 @@ def elogout():
     else:
         return not_found()
 
+
+@app.route('/employee/signup',methods=['POST'])
+def eSignup():
+
+    if request.method == 'POST':
+
+        ejwt = request.cookies.get("EJWT",None)
+
+        if(ejwt == None):
+            return authenticationError()
+
+        try:
+            j = jwt.decode(ejwt, secret, algorithms=["HS256"])
+        except:
+            return authenticationError()
+
+        #a valid ejwt was given
+        #checks whether this employee has permission or not
+        if(j["loggedIn"] == False or j["eType"] != "Admin"):
+            return authenticationError()
+
+        #Permissions are granted
+
+        #Gets required attributes from JSON body
+        try:
+            _json = request.json
+            _branch = _json["branchId"]
+            _email = _json['email']
+            _phone = _json['phoneNum']
+            _dob = _json['dob']
+            _fName = _json["firstName"]
+            _lName = _json["lastName"]
+            _sex = _json["sex"]
+            _eT = _json["eType"]
+            _pw = _json["password"]
+        except:
+            return badRequest()
+
+        #We have all the attributes
+
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+
+            #hash
+            pb = str.encode(_pw)
+            h1 = bcrypt.hashpw(pb, salt) #No random salt used for simplification
+            h = h1.decode()
+
+            sqlQuery = "INSERT INTO employee(branchId,email,phoneNum,dob,firstName,lastName,sex,eType, passwordHash) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);"
+            bindData = (_branch,_email, _phone, _dob,_fName,_lName,_sex,_eT,h)
+
+            cursor.execute(sqlQuery, bindData)
+            conn.commit()
+
+            result = cursor.execute('SELECT eId,branchId,email,phoneNum,DATE_FORMAT(dob,"%Y-%m-%d") as dob,firstName,lastName,sex,eType FROM employee WHERE eId = LAST_INSERT_ID();')
+
+
+
+            if(result <= 0):
+                print("SQL ERROR - No Return")
+                return serverError()
+
+
+            r = cursor.fetchone()
+
+            response = jsonify(r)
+            response.status_code = 200
+            return response
+
+        except:
+            return badRequest()
+        finally:
+            conn.close()
+            cursor.close()
+
+
+
+    else:
+        return not_found()
+
+
+
+
 @app.route('/client/login',methods=['POST'])
 def clogin():
 
@@ -115,7 +200,7 @@ def clogin():
         _id = _json['clientId']
         _pw = _json['password']
     except:
-        return authenticationError()
+        return badRequest()
 
     if(type(_id) != int or type(_pw) != str):
         return authenticationError()
@@ -129,7 +214,7 @@ def clogin():
         try:
             conn = mysql.connect()
             cursor = conn.cursor(pymysql.cursors.DictCursor)
-            result = cursor.execute(f'SELECT clientId,email,phoneNum,dob,firstName,lastName,sex,memberType,price,startDate,endDate FROM client WHERE clientId={_id} AND passwordHash="{h}";')
+            result = cursor.execute(f'SELECT clientId,email,phoneNum,DATE_FORMAT(dob,"%Y-%m-%d") as dob,firstName,lastName,sex,memberType,price,startDate,endDate FROM client WHERE clientId={_id} AND passwordHash="{h}";')
             if(result <= 0):
                 return authenticationError()
             else: #found
@@ -192,6 +277,86 @@ def clogout():
     else:
         return not_found()
 
+@app.route('/client/signup',methods=['POST'])
+def cSignup():
+
+    if request.method == 'POST':
+
+        ejwt = request.cookies.get("EJWT",None)
+
+        if(ejwt == None):
+            return authenticationError()
+
+        try:
+            j = jwt.decode(ejwt, secret, algorithms=["HS256"])
+        except:
+            return authenticationError()
+
+        #a valid ejwt was given
+        #checks whether this employee has permission or not
+        if(j["loggedIn"] == False or j["eType"] != "Admin"):
+            return authenticationError()
+
+        #Permissions are granted
+
+        #Gets required attributes from JSON body
+        try:
+            _json = request.json
+            _email = _json['email']
+            _phone = _json['phoneNum']
+            _dob = _json['dob']
+            _fName = _json["firstName"]
+            _lName = _json["lastName"]
+            _sex = _json["sex"]
+            _mT = _json["memberType"]
+            _price = _json["price"]
+            assert(type(_price) == float)
+            _startDate = _json["startDate"]
+            _endDate = _json["endDate"]
+            _pw = _json["password"]
+        except:
+            return badRequest()
+
+        #We have all the attributes
+
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+            #hash
+            pb = str.encode(_pw)
+            h1 = bcrypt.hashpw(pb, salt) #No random salt used for simplification
+            h = h1.decode()
+
+            sqlQuery = "INSERT INTO client(email,phoneNum,dob,firstName,lastName,sex,memberType,price,startDate,endDate, passwordHash) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+            bindData = (_email, _phone, _dob,_fName,_lName,_sex,_mT,_price,_startDate,_endDate,h)
+
+            cursor.execute(sqlQuery, bindData)
+            conn.commit()
+
+            result = cursor.execute('SELECT clientId,email,phoneNum,DATE_FORMAT(dob,"%Y-%m-%d") as dob,firstName,lastName,sex,memberType,price,DATE_FORMAT(startDate,"%Y-%m-%d") as startDate,DATE_FORMAT(endDate,"%Y-%m-%d") as endDate FROM client WHERE LAST_INSERT_ID() = clientId;')
+
+            if(result <= 0):
+                return serverError()
+
+
+            r = cursor.fetchone()
+
+            response = jsonify(r)
+            response.status_code = 200
+            return response
+
+        except:
+            return badRequest()
+        finally:
+            conn.close()
+            cursor.close()
+
+
+
+    else:
+        return not_found()
+
 
 
 #Occurs when request cannot be satisfied
@@ -205,11 +370,29 @@ def not_found(error=None):
     respone.status_code = 404
     return respone
 
+def serverError():
+    message = {
+        'status': 500,
+        'message': "Something Bad happened and I don't know what",
+    }
+    respone = jsonify(message)
+    respone.status_code = 500
+    return respone
 
 def authenticationError():
     message = {
         'status': 400,
         'message': 'Authentication Error',
+    }
+    respone = jsonify(message)
+    respone.status_code = 400
+    return respone
+
+
+def badRequest():
+    message = {
+        'status': 400,
+        'message': 'Bad Request',
     }
     respone = jsonify(message)
     respone.status_code = 400
