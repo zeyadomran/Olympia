938b4307b25c17fd7c30f70182820e443aa44598
Austin Shawaga <60626400+Retr0-XX@users.noreply.github.com>
2022-03-22 21:14:36 -0600

    Added Employee and Client Log in

--- BackEnd/API/main.py
+++ BackEnd/API/main.py
@@ -15,32 +15,111 @@ from app import app
 from config import mysql
 from flask import jsonify
 from flask import flash, request
+import jwt
+import bcrypt
+
+secret = "!!1234OlympiaGymDatabase!5678!"
+salt = b'$2b$12$bHop6pQFFJBG86NnXZUg4.'
 
 
-@app.route('/CLIENT')
-def getClients():
+@app.route('/employee/login',methods=['POST'])
+def elogin():
+
+    try:
+        _json = request.json
+        _id = _json['eId']
+        _pw = _json['password']
+    except:
+        return authenticationError()
+
+    if(type(_id) != int or type(_pw) != str):
+        return authenticationError()
+
+    if _id and _pw and request.method == 'POST':
+        #hash
+        pb = str.encode(_pw)
+        h1 = bcrypt.hashpw(pb, salt) #No random salt used for simplification
+        h = h1.decode()
+
         try:
             conn = mysql.connect()
             cursor = conn.cursor(pymysql.cursors.DictCursor)
-            result = cursor.execute("SELECT * FROM CLIENT;")
-            print("Result:",result)
+            result = cursor.execute(f'SELECT eId,branchId,email,phoneNum,dob,firstName,lastName,sex,eType FROM employee WHERE eId={_id} AND passwordHash="{h}";')
+            if(result <= 0):
+                return authenticationError()
+            else: #found
+                emp = cursor.fetchone()
 
-            #If nothing was returned send back a 404
-            if (result <= 0):
-                print("No Results") #This occurs when response comes back empty
-                return not_found()
-            else: #Something was found
-                empRows = cursor.fetchall()
-                respone = jsonify(empRows)
-                respone.status_code = 200
-                return respone
+                m = {"eId": _id,"eType":emp["eType"]}
 
+                j = jwt.encode(m, secret, algorithm="HS256")
+
+                print("Made EJWT with id:",_id,"and Type:",emp["eType"]) #debugging purposes
+
+                response = jsonify(emp)
+                response.status_code = 200
+
+                #set cookie
+                response.set_cookie("EJWT", j, max_age=604800) #cookie expires in a week
+
+                return response
         except Exception as e:
-            print(e)
-            serverError()
+            print("ERROR:",e)
         finally:
             cursor.close()
             conn.close()
+    else:
+        return authenticationError()
+
+
+@app.route('/client/login',methods=['POST'])
+def clogin():
+
+    try:
+        _json = request.json
+        _id = _json['clientId']
+        _pw = _json['password']
+    except:
+        return authenticationError()
+
+    if(type(_id) != int or type(_pw) != str):
+        return authenticationError()
+
+    if _id and _pw and request.method == 'POST':
+        #hash
+        pb = str.encode(_pw)
+        h1 = bcrypt.hashpw(pb, salt) #No random salt used for simplification
+        h = h1.decode()
+
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            result = cursor.execute(f'SELECT clientId,email,phoneNum,dob,firstName,lastName,sex,memberType,price,startDate,endDate FROM client WHERE clientId={_id} AND passwordHash="{h}";')
+            if(result <= 0):
+                return authenticationError()
+            else: #found
+                cli = cursor.fetchone()
+
+                m = {"clientId": _id}
+
+                j = jwt.encode(m, secret, algorithm="HS256")
+
+                print("Made CJWT with id:",_id) #debugging purposes
+
+                response = jsonify(cli)
+                response.status_code = 200
+
+                #set cookie
+                response.set_cookie("CJWT", j, max_age=604800) #cookie expires in a week
+
+                return response
+        except Exception as e:
+            print("ERROR:",e)
+        finally:
+            cursor.close()
+            conn.close()
+    else:
+        return authenticationError()
 
 
 #Occurs when request cannot be satisfied
@@ -55,15 +134,15 @@ def not_found(error=None):
     return respone
 
 
-#Used when exceptions are encountered
-def serverError():
+def authenticationError():
     message = {
-        'status': 500,
-        'message': 'Internal Server Error',
+        'status': 400,
+        'message': 'Authentication Error',
     }
     respone = jsonify(message)
-    respone.status_code = 500
+    respone.status_code = 400
     return respone
+
 
 #Launches the app
 if __name__ == "__main__":
