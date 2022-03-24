f9e2dcdd34c2121797d6b22bab174df6b4b98118
zeyadomran <ziomran@gmail.com>
2022-03-23 20:05:22 -0600

    Created Index Page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,13 +1,33 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import Head from "next/head";
 
 const Home: NextPage = () => {
+	const router = useRouter();
 	return (
 		<>
 			<Head>
 				<title>Olympia</title>
 			</Head>
-			<p className="text-3xl font-bold">Olympia</p>
+			<div className="w-full h-screen flex justify-center items-center bg-white-2">
+				<div className="p-8 h-80 w-72 bg-white flex flex-col justify-start items-center rounded-xl shadow-md">
+					<h1 className="text-4xl font-bold">Olympia</h1>
+					<div className="w-full flex flex-col justify-center items-center gap-y-4 mt-16">
+						<button
+							className="py-4 w-4/5 text-xl text-white font-medium bg-blue hover:bg-blue-2 transition-all rounded-xl"
+							onClick={() => router.push("/login")}
+						>
+							Client Login
+						</button>
+						<button
+							className="py-4 w-4/5 text-xl text-white font-medium bg-blue hover:bg-blue-2 transition-all rounded-xl"
+							onClick={() => router.push("/employee/login")}
+						>
+							Admin Login
+						</button>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 };
